d8f198569038504d6a856c6fd575ad2f29c55f7d
Yosua Peitho Purba <yosuapeitho2@gmail.com>
2026-09-14 13:17:18 +0700

    Add show_education view and url pattern

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from main.models import Experience
+from main.models import Experience, Education 
 
 def show_main(request):
     context = {
@@ -19,3 +19,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    education_list = Education.objects.all()
+    context = {
+        'name': 'Yosua Peitho Purba',
+        'education_list': education_list,
+    }
+    return render(request, 'education.html', context)
